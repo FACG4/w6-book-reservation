617395c480b03fd47fc10b926d90336b7dd15812
Farah Zaqout <farah.zaqout@gmail.com>
2018-04-17 16:44:16 +0300

    fixed all issues

--- src/database/db_build.sql
+++ src/database/db_build.sql
@@ -5,7 +5,7 @@ DROP TABLE IF EXISTS books, reservation, users CASCADE;
 CREATE TABLE books(
 id SERIAL PRIMARY KEY,
 name VARCHAR(50) UNIQUE,
-author VARCHAR(50),
+author VARCHAR(50)
 );
 
 CREATE TABLE users(
@@ -21,7 +21,7 @@ CREATE TABLE reservation(
 book_id INTEGER REFERENCES books(id),
 user_id INTEGER REFERENCES users(id),
 start_date DATE NOT NULL,
-end_date DATE NOT NULL,
+end_date DATE NOT NULL
 );
 
 COMMIT;
