c101fbbf7bf3d717ac497d2460904d93f295d68e
eman <eman_h_2009@live.com>
2018-04-18 11:45:26 +0300

    return select data from bake end

--- src/handler.js
+++ src/handler.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const getData =require('./database/queries/get')
 
 
 const contentType = {
@@ -22,6 +23,24 @@ const servePublic = (endpoint, res) => {
      res.end(file);
    }
  })
+};
+
+const selectData = (req,res)=>{
+  let data = '';
+  req.on("data",(chunck)=>{
+    data += chunck
+  });
+  req.on('end',()=>{
+    data=JSON.parse(data);
+    console.log(data.value);
+    
+    getData(data.value , (err,result)=>{
+      if(err) throw new Error(err);
+      res.writeHead(200,{"Content-Type":"application/json"});
+      res.end(JSON.stringify(result))
+    })
+  })
+  
 }
 
-module.exports= {servePublic};
+module.exports= {servePublic,selectData };
