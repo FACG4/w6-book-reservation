11558c3543f6624a89e04428417e9abe40f30e9b
Farah Zaqout <farah.zaqout@gmail.com>
2018-04-18 15:15:41 +0300

    fixed the like and case insensetive issue

--- src/database/queries/get.js
+++ src/database/queries/get.js
@@ -2,8 +2,8 @@ const connection = require('../db_connect');
 
 const getData = (search_value,cb)=>{
 const sql ={
-  text :"SELECT * FROM books WHERE name ilike $1",
-  values: [search_value]
+  text :'SELECT * FROM books WHERE name ilike $1',
+  values: ['%' + search_value + '%']
 } ;
 connection.query(sql,(err,res)=>{
   if(err){
