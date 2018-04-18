088fbf0207cafc286b9dbf6847ce6458f517eb1d
Farah Zaqout <farah.zaqout@gmail.com>
2018-04-18 14:11:29 +0300

    fixed conflict with get.js

---
+++ src/database/queries/get.js
@@ -0,0 +1,17 @@
+const connection = require('../db_connect');
+
+const getData = (search_value,cb)=>{
+const sql ={
+  text :"SELECT * FROM books WHERE name =$1",
+  values: [search_value]
+} ;
+connection.query(sql,(err,res)=>{
+  if(err){
+   return cb(err)
+}
+    cb(null,res.rows);
+
+});
+}
+
+module.exports=getData;
