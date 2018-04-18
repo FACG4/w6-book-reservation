7bcca629f1dd41b66dad971b171c6370fbb4a731
eman <eman_h_2009@live.com>
2018-04-18 13:11:19 +0300

    add input field in the dom

--- public/js/dom.js
+++ public/js/dom.js
@@ -2,17 +2,33 @@
 function select (selector){
   return document.querySelector(selector);
 }
+
+
 var search = select(".search_btn");
 var input = select(".search_book");
 
-
-console.log(search);
 search.addEventListener("click", function(){
 var name_book = input.value;
 
 fetch('/getData','POST' ,name_book,function(res){
- console.log(res);
- 
+  console.log(res);
+  var startDateInput = document.createElement('input');
+  startDateInput.setAttribute('type', 'date');
+  var endDateInput = document.createElement('input');
+  endDateInput.setAttribute('type', 'date');
+  var reserveButton = document.createElement('input');
+  reserveButton.setAttribute('type', 'button');
+  var li = document.createElement('li');
+  li.textContent = res.name;
+  var oldLi = select('#book');
+  var ul = document.createElement('ul');
+  ul.appendChild(li);
+  ul.appendChild(startDateInput);
+  ul.appendChild(endDateInput);
+  ul.appendChild(reserveButton);
+  select('#bookContainer').replaceChild(ul, oldLi)
+  
+
 })
 
 
--- src/handler.js
+++ src/handler.js
@@ -33,12 +33,14 @@ const selectData = (req,res)=>{
   });
   req.on('end',()=>{
     data=JSON.parse(data);
-    console.log(data.value);
-    
+  //console.log(data.value);
+  
     getData(data.value , (err,result)=>{
       if(err) throw new Error(err);
       res.writeHead(200,{"Content-Type":"application/json"});
-      res.end(JSON.stringify(result))
+      console.log(typeof result);
+      
+      res.end(JSON.stringify(result[0]))
     })
   })
   
